44fc025d8b6307efb92bc25e52fec60b82b5bebc
Chan <channyein.k@gmail.com>
2023-05-09 18:42:32 +0700

    chore: setup tailwind plugins line-clamp, aspect-ratio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,5 +17,9 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
