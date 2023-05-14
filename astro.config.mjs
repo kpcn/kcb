b4b27d0f19321991e7974ef7050d1a3081bc9aaf
Chan <channyein.k@gmail.com>
2023-05-14 17:10:28 +0700

    feat: setup sitemap generation with @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,26 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
-import { astroImageTools } from 'astro-imagetools';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), astroImageTools, image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx({})]
+  site: 'https://kcnk.me',
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    mdx({}),
+    sitemap(),
+  ],
 });
