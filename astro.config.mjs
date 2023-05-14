062eae42e935c45aa21e60c64e59f89123fba70a
Chan <channyein.k@gmail.com>
2023-05-14 16:45:00 +0700

    refactor: image optimization with astro-imagetools integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
+import { astroImageTools } from 'astro-imagetools';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx({}),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), astroImageTools, image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx({})]
 });
