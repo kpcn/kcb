d33125aef3ab622318c4a09f5ce9961967c7ad22
Chan <channyein.k@gmail.com>
2023-05-16 15:39:25 +0700

    feat: changed the build config for vercel serverless to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from '@astrojs/vercel/static';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
 import sitemap from '@astrojs/sitemap';
@@ -25,6 +25,6 @@ export default defineConfig({
     mdx({}),
     sitemap(),
   ],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercelStatic(),
 });
