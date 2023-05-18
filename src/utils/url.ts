4945fff42196082f78e8ce7a9ed3fe588b6be0e2
Chan <channyein.k@gmail.com>
2023-05-18 17:32:16 +0700

    fix: reponsive font sizes for header related components

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,17 +1,23 @@
 import { kebabCaseToText } from './format';
 
 export const isBlogRelatedUrl = (pathname: string) => {
-  return pathname.match(/\/blog\//g);
+	return pathname.match(/\/blog\//g);
 };
 
 export const isTagsPage = (pathname: string) => {
-  return pathname.match(/\/tags\//g);
+	return pathname.match(/\/blog\/tags\//g);
+};
+
+export const isBlogUrl = (pathname: string) => {
+	if (pathname.match(/\/blog\/[a-zA-Z0-9-]+\/$/g)) {
+		return true;
+	}
 };
 
 export const getTagName = (url: string, defaultReturnString = 'All') => {
-  const name = url.split('/').slice(-2)[0];
-  if (!isBlogRelatedUrl(url)) {
-    return '';
-  }
-  return kebabCaseToText(name) || defaultReturnString;
+	const name = url.split('/').slice(-2)[0];
+	if (!isBlogRelatedUrl(url)) {
+		return '';
+	}
+	return kebabCaseToText(name) || defaultReturnString;
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,44 +2,44 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        dacingscript: ['Dancing Script', 'cursive'],
-        firamono: ['Fira Mono', 'monospace'],
-      },
-      screens: {
-        xs: '475px',
-        ...defaultTheme.screens,
-      },
-      typography: (theme) => {
-        return {
-          DEFAULT: {
-            css: {
-              'code::before': { content: '' },
-              'code::after': { content: '' },
-              a: {
-                fontColor: theme('colors.blue.500'),
-              },
-              pre: {
-                code: {
-                  backgroundColor: 'transparent',
-                },
-                'code:not(.not-prose)': {
-                  backgroundColor: 'transparent',
-                },
-              },
-            },
-          },
-        };
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				dacingscript: ['Dancing Script', 'cursive'],
+				firamono: ['Fira Mono', 'monospace'],
+			},
+			screens: {
+				xs: '475px',
+				...defaultTheme.screens,
+			},
+			typography: (theme) => {
+				return {
+					DEFAULT: {
+						css: {
+							'code::before': { content: '' },
+							'code::after': { content: '' },
+							a: {
+								fontColor: theme('colors.blue.500'),
+							},
+							pre: {
+								code: {
+									backgroundColor: 'transparent',
+								},
+								'code:not(.not-prose)': {
+									backgroundColor: 'transparent',
+								},
+							},
+						},
+					},
+				};
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/aspect-ratio'),
+	],
 };
