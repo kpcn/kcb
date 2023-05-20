152849d0c6b859ba810f15b4b52d9956d4780dcc
Chan <channyein.k@gmail.com>
2023-05-20 20:24:23 +0700

    chore: updated config for vitest and package.json & change eslintrc format to cjs

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -15,22 +15,3 @@ export const blogSchema = z.object({
 	relatedPosts: z.array(z.string()).optional(),
 });
 
-// EXAMPLE: example YAML snippet for frontmatter
-// title: 'Post 01'
-// publishedAt: '2022-09-12'
-// summary: 'Eu sint esse aute eiusmod officia eu ut sit dolor.'
-// excerpt: 'Ad sit irure voluptate culpa ea exercitation do pariatur commodo qui amet cillum minim.Esse est commodo fugiat esse cupidatat excepteur consequat excepteur fugiat velit sint mollit...'
-// image:
-//   - provider: 'unsplash'
-//     config:
-//       photoId: 'cYUMaCqMYvI'
-//   - provider: 'local'
-//     config:
-//       url: '/images/opposite-of-upward-arrows.webp'
-//       alt: 'Upward arrows (Opposite directions)'
-//       credit: 'Jungwoo Hong'
-//       creditLink: 'https://unsplash.com/photos/cYUMaCqMYvI'
-// tags:
-//   - Resource Hints
-//   - WPO
-//   - React
