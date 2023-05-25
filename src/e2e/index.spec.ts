82046d4b1a4b12ea53d475e4e6d1370841e0483b
kpcn <channyein.k@gmail.com>
2023-05-25 20:25:36 +0700

    test: basic & simple tests for home page

--- src/e2e/index.spec.ts
+++ src/e2e/index.spec.ts
@@ -1,8 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-test('meta title is correct', async ({ page }) => {
-  const defaultTitle = "KC's Blog";
-  await page.goto('http://localhost:3000/');
+test.describe('Home page', () => {
+	test('default meta data are correct', async ({ page }) => {
+		const defaultTitle = "KC's Blog";
+		await page.goto('/');
 
-  await expect(page).toHaveTitle(defaultTitle);
+		const metaDescription = page.locator('meta[name="description"]');
+
+		await expect(page).toHaveTitle(defaultTitle);
+		await expect(metaDescription).toHaveAttribute(
+			'content',
+			'Some thoughts on the things I like, especially on Web Dev'
+		);
+	});
+
+	test('avatar source is correct', async ({ page }) => {
+		await page.goto('/');
+		const avatar = page.locator('img[alt="KC"]');
+		await expect(avatar).toHaveAttribute('src', /avatar.png/);
+	});
+	test('links are present in contentinfo', async ({ page }) => {
+		await page.goto('/');
+		const links = page.getByRole('contentinfo').locator('a');
+		await expect(links).toHaveCount(2);
+	});
+
+	test('blog posts are rendered', async ({ page }) => {
+		await page.goto('/');
+		const postCount = await page.getByRole('listitem').count();
+		expect(postCount).toBeGreaterThan(0);
+	});
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	test.fixme('blog posts are sorted by date', () => {});
 });
